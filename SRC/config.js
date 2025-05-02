91d7ef937ce5d252a8f517b3afa9d315c0ec043f
psahx <ovals.coney_5t@icloud.com>
2025-05-02 16:33:05 -0700

    Create config.js

---
+++ SRC/config.js
@@ -0,0 +1,14 @@
+// SRC/config.js
+'use strict';
+
+/**
+ * Exports the main configuration object for the plugin.
+ */
+export const config = {
+    api_url: 'https://api.mdblist.com/tmdb/', // Base URL for MDBList TMDB endpoint
+    // api_key is configured via Lampa Settings -> Additional Ratings
+    cache_time: 60 * 60 * 12 * 1000, // 12 hours cache duration
+    cache_key: 'mdblist_ratings_cache', // Unique storage key for ratings data
+    cache_limit: 500, // Max items in cache
+    request_timeout: 10000 // 10 seconds request timeout
+};
