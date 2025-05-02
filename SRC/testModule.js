e8204bd145ad0ab13c33920fcd52f0d8d4b6fada
psahx <ovals.coney_5t@icloud.com>
2025-05-02 16:14:27 -0700

    Create testModule.js

---
+++ SRC/testModule.js
@@ -0,0 +1,7 @@
+// This file exports a simple value.
+console.log('testModule.js: Executing'); // To see if the file itself is loaded
+
+export const esm_test_value = 'ES Module Test Successful!';
+
+// Log success of module execution itself
+console.log('testModule.js: Export defined');
