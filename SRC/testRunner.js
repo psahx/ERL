69a85f25777419d4748ecf2b83b8339d4485c493
psahx <ovals.coney_5t@icloud.com>
2025-05-02 16:16:05 -0700

    Create testRunner.js

---
+++ SRC/testRunner.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    console.log('testRunner.js: Starting ES Module Test');
+    Lampa.Noty.show('Starting ES Module Test...'); // Visual feedback in Lampa
+
+    // **Replace with the ACTUAL URL where you hosted testModule.js**
+    const moduleUrl = 'URL_TO_YOUR_HOSTED_testModule.js';
+
+    // Try dynamic import
+    import(moduleUrl)
+        .then(module => {
+            console.log('testRunner.js: Dynamic import successful!');
+            console.log('testRunner.js: Imported value:', module.esm_test_value);
+            // Display success in Lampa UI
+            Lampa.Noty.show('Success: ' + module.esm_test_value, { time: 10000 });
+        })
+        .catch(error => {
+            console.error('testRunner.js: Dynamic import failed:', error);
+            // Display failure in Lampa UI
+            Lampa.Noty.show('ES Module Test FAILED: ' + error.message, { time: 10000 });
+        });
+
+})();
