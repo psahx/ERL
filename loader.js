b8d193bbb56043085696ebb1f71081feb3bc6263
psahx <ovals.coney_5t@icloud.com>
2025-05-02 22:16:59 -0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 // In loader.js (root folder)
 const mainScriptUrl = 'https://psahx.github.io/ERL/SRC/main.js'; // Replace with actual URL
 Lampa.Utils.putScriptAsync([mainScriptUrl], function () {
-    console.log('Loader: Main script loaded.');
+    console.log("PsahxRatingsPlugin: Executing loader.js");
 });
