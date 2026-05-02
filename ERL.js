5ac0b60c2f5bb6e0ecafe54a16838777f7c4bb6a
psahx <ovals.coney_5t@icloud.com>
2026-05-01 23:51:19 -0700

    Update script to run on the updated LAMPA APP.
    Rewrote whole thing to utilize lampa's original css styles, without injecting any styles. Much lighter and cleaner layout.
    
    Added ratings in the card itself as well.

--- ERL.js
+++ ERL.js
@@ -1,4 +1,4 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://psahx.github.io/ps_plug/MDBList.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://psahx.github.io/ps_plug/WideCard.js'], function () { });
 })();
